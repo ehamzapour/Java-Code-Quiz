b944562d7549dc82d043db4dfa669e9adf7b7a27
Elham Hamzapour <ehamzapour@gmail.com>
2022-08-25 12:49:21 -0500

    finished score.js, may need to make changes later

--- score.js
+++ score.js
@@ -6,7 +6,25 @@ var clearBtn = document.querySelector("#clearScores");
 function displayScores() {
     if (savedScores !== null) {
         var scoreList = document.createElement("ol");
-        scoreList.className = "scoreListt";
-        
+        scoreList.className = "highScoreClass";
+        for (var i=0; i < savedScores.length; i++){
+            var initials = savedScores[i].inits;
+            var scores = savedScores[i].userScore
+            var scoreName = document.createElement("li");
+            scoreName.innerHTML = initials + "-" + scores;
+            scoreList.appendChild(scoreName);
+        }
+        scoresList.appendChild(scoreList);
     }
-}
+};
+
+displayScores();
+
+backBtn.addEventListener("click", function(){
+    location.href = "index.html";
+});
+
+clearBtn.addEventListener("click", function(){
+    scoresList.innerHTML = "";
+    window.localStorage.clear();
+});
